36189a0ce58bb582b17b1441108de736348ba4fb
BoyGK <1215356195@qq.com>
2017-06-08 20:37:44 +0800

    bgq

--- app/src/main/java/com/example/a1111/term4_homework/student/second/SecondItemListFragment.java
+++ app/src/main/java/com/example/a1111/term4_homework/student/second/SecondItemListFragment.java
@@ -1,30 +1,39 @@
 package com.example.a1111.term4_homework.student.second;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 import com.example.a1111.term4_homework.R;
+import com.example.a1111.term4_homework.interface_.HttpCallbackListener;
+import com.example.a1111.term4_homework.model.STHomeModel;
+import com.example.a1111.term4_homework.student.first.dummy.DummyContent;
+import com.example.a1111.term4_homework.util.DataUtil;
+import com.example.a1111.term4_homework.util.HttpUtils;
+import com.example.a1111.term4_homework.util.L;
+import com.google.gson.Gson;
 
-import com.example.a1111.term4_homework.student.second.dummy.DummyContent;
-
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 /**
- * An activity representing a list of SecondItems. This activity
+ * An activity representing a list of FirstItems. This activity
  * has different presentations for handset and tablet-size devices. On
  * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link SecondItemDetailActivity} representing
+ * lead to a {@link SecondItemListFragment} representing
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
@@ -35,11 +44,13 @@ public class SecondItemListFragment extends Fragment {
      * device.
      */
     private boolean mTwoPane;
+    View recyclerView;
+    SimpleItemRecyclerViewAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_seconditem_list_student,container);
+        View view = inflater.inflate(R.layout.fragment_seconditem_list_student, container);
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,11 +61,9 @@ public class SecondItemListFragment extends Fragment {
             }
         });
 
-        View recyclerView = view.findViewById(R.id.seconditem_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+        recyclerView = view.findViewById(R.id.seconditem_list);
 
-        if (view.findViewById(R.id.seconditem_detail_container) != null) {
+        if (view.findViewById(R.id.student_firstitem_detail_container) != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
             // If this view is present, then the
@@ -62,19 +71,53 @@ public class SecondItemListFragment extends Fragment {
             mTwoPane = true;
         }
 
+        getData();
+
         return view;
     }
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    //
+    private void getData() {
+        HashMap<String, String> map = new HashMap<>();
+        DataUtil util = new DataUtil("userinformation", getActivity());
+        map.put("userid", util.getData("userid", ""));
+        HttpUtils.get("http://183.175.12.176:8080/getwvideo", map, new HttpCallbackListener.StringCallBack() {
+            @Override
+            public void OnRequest(String response) {
+                STHomeModel model = new Gson().fromJson(response, STHomeModel.class);
+                if (model.getState() != 0) {
+                    adapter = new SimpleItemRecyclerViewAdapter(model.getVideos());
+                    callback.success();
+                } else {
+                    L.e("home", model.getMsg());
+                }
+            }
+
+            @Override
+            public void OnFailure(Exception e) {
+                L.e("home", e.getLocalizedMessage());
+            }
+        });
     }
+
+
+    interface Callback {
+        void success();
+    }
+
+    Callback callback = new Callback() {
+        @Override
+        public void success() {
+            ((RecyclerView) recyclerView).setAdapter(adapter);
+        }
+    };
 
     public class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final List<DummyContent.DummyItem> mValues;
+        private final List<STHomeModel.VideosBean> mValues;
 
-        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+        public SimpleItemRecyclerViewAdapter(List<STHomeModel.VideosBean> items) {
             mValues = items;
         }
 
@@ -86,26 +129,27 @@ public class SecondItemListFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
             holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
+            holder.mIdView.setText(mValues.get(position).getVideoid() + "");
+            holder.mContentView.setText("标题" + mValues.get(position).getTitle()
+                    + "  课程" + mValues.get(position).getSubject());
 
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (mTwoPane) {
                         Bundle arguments = new Bundle();
-                        arguments.putString(SecondItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        arguments.putSerializable("video", mValues.get(position));
                         SecondItemDetailFragment fragment = new SecondItemDetailFragment();
                         fragment.setArguments(arguments);
                         getActivity().getFragmentManager().beginTransaction()
-                                .replace(R.id.seconditem_detail_container, fragment)
+                                .replace(R.id.student_seconditem_container, fragment)
                                 .commit();
                     } else {
                         Context context = v.getContext();
                         Intent intent = new Intent(context, SecondItemDetailActivity.class);
-                        intent.putExtra(SecondItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        intent.putExtra(SecondItemDetailFragment.ARG_ITEM_ID, holder.mItem.getVideoid() + "");
 
                         context.startActivity(intent);
                     }
@@ -122,13 +166,13 @@ public class SecondItemListFragment extends Fragment {
             public final View mView;
             public final TextView mIdView;
             public final TextView mContentView;
-            public DummyContent.DummyItem mItem;
+            public STHomeModel.VideosBean mItem;
 
             public ViewHolder(View view) {
                 super(view);
                 mView = view;
-                mIdView = (TextView) view.findViewById(R.id.student_firstitem_list_content_id);
-                mContentView = (TextView) view.findViewById(R.id.student_firstitem_list_content_main_content);
+                mIdView = (TextView) view.findViewById(R.id.student_seconditem_list_content_id);
+                mContentView = (TextView) view.findViewById(R.id.student_seconditem_list_content_main_content);
             }
 
             @Override
